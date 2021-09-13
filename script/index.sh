1f31f77c81a10e87bd8076551c02aeb53e7dc5be
Ms Kimsible <kimsible@users.noreply.github.com>
2021-09-14 00:12:05 +0200

    Use standard printf

--- script/index.sh
+++ script/index.sh
@@ -58,7 +58,7 @@ has() {
 
 curlf() {
   STATUSCODE=`curl -sSL --write-out "%{http_code}" $1 -o $2`
-  [ "$STATUSCODE" -ne 200 ] && echo $ERROR && echo "${ORANGE}HTTP $STATUSCODE${NC} ${RED}$1${NC}" && exit 1
+  [ "$STATUSCODE" -ne 200 ] && printf "%b" "$ERROR\n" && printf "%b" "${ORANGE}HTTP $STATUSCODE${NC} ${RED}$1${NC}\n" && exit 1
 }
 
 # Get latest file from GitHub raw
@@ -140,11 +140,11 @@ validate_email() {
 }
 
 prompt() {
-  read -p "What is your $1? " INPUT
+  read -p " What is your $1? " INPUT
 
   while [ -z "`validate_$1 $INPUT`" ]; do
-    echo "${ERROR}: ${ORANGE}$INPUT${NC} is not a valid $1, please enter a valid one" >&2 # prompt error after failed
-    read -p "What is your $1? " INPUT
+    printf "%b" " ${ERROR}: ${ORANGE}$INPUT${NC} is not a valid $1, please enter a valid one\n" >&2 # prompt error after failed
+    read -p " What is your $1? " INPUT
   done
 
   echo $INPUT
@@ -155,7 +155,7 @@ prompt() {
 #################
 
 # Step 1
-echo "Prerequisites \\"
+printf "%b" "Prerequisites\n"
 missing_prerequisites=0
 
 # root
@@ -163,38 +163,38 @@ uid=`id -u`
 if [ "$uid" -ne 0 ]; then
   missing_prerequisites=1
   sudo_command="curl https://getpeer.tube -o getpt.sh && sudo -E sh getpt.sh"
-  echo " $ERROR: must be run as root or sudoer user with ${ORANGE}$sudo_command${NC}"
+  printf "%b" " $ERROR: must be run as root or sudoer user with ${ORANGE}$sudo_command${NC}\n"
 else
-  echo " root $OK"
+  printf "%b" " root $OK\n"
 fi
 
 # systemd
 if ! has "systemctl"; then
   missing_prerequisites=1
-  echo " $ERROR:systemd is missing"
+  printf "%b" " $ERROR:systemd is missing\n"
 else
-  echo " systemd $OK"
+  printf "%b" " systemd $OK\n"
 fi
 
 # curl
 if ! has "curl"; then
   missing_prerequisites=1
-  echo " $ERROR: curl is missing"
+  printf "%b" " $ERROR: curl is missing\n"
 else
-  echo " curl $OK"
+  printf "%b" " curl $OK\n"
 fi
 
 # docker
 if ! has "docker"; then
   missing_prerequisites=1
-  echo " $ERROR: docker >= $DOCKER_PREREQUISITE_RELEASE is missing"
+  printf "%b" " $ERROR: docker >= $DOCKER_PREREQUISITE_RELEASE is missing\n"
 else
   docker_current_release=`get_current_release "docker -v"`
   if ! is_update "$docker_current_release" "$DOCKER_PREREQUISITE_RELEASE"; then
     missing_prerequisites=1
-    echo " $ERROR: docker >= $DOCKER_PREREQUISITE_RELEASE is required, found $docker_current_release"
+    printf "%b" " $ERROR: docker >= $DOCKER_PREREQUISITE_RELEASE is required, found $docker_current_release\n"
   else
-    echo " docker $OK"
+    printf "%b" " docker $OK\n"
   fi
 fi
 
@@ -202,7 +202,7 @@ fi
 if [ ! -z "$MY_DOMAIN" ]; then
   if [ -z "`validate_domain $MY_DOMAIN`" ]; then
     missing_prerequisites=1
-    echo " $ERROR: ${ORANGE}$MY_DOMAIN${NC} is not a valid domain"
+    printf "%b" " $ERROR: ${ORANGE}$MY_DOMAIN${NC} is not a valid domain\n"
   fi
 fi
 
@@ -210,7 +210,7 @@ fi
 if [ ! -z "$MY_EMAIL_ADDRESS" ]; then
   if [ -z "`validate_email $MY_EMAIL_ADDRESS`" ]; then
     missing_prerequisites=1
-    echo " $ERROR: ${ORANGE}$MY_EMAIL_ADDRESS${NC} is not a valid email"
+    printf "%b" " $ERROR: ${ORANGE}$MY_EMAIL_ADDRESS${NC} is not a valid email\n"
   fi
 fi
 
@@ -220,8 +220,8 @@ if [ "$missing_prerequisites" -ne 0 ]; then exit 1; fi
 # Check if a stack is alreay installed
 if [ -f $WORKDIR/.env ] || [ -f $WORKDIR/docker-compose.yml ] || [ -f $WORKDIR/docker-volume ]; then
   # Step 2
-  echo "\n${ORANGE}Docker stack already exists in $WORKDIR${NC}"
-  echo "\nUpgrading ${GREEN}Compose${NC} and ${GREEN}CLI${NC} only \\"
+  printf "%b" "\n${ORANGE}Docker stack already exists in $WORKDIR${NC}\n\n"
+  printf "%b" "\nUpgrading ${GREEN}Compose${NC} and ${GREEN}CLI${NC} only \ \n"
   UPGRADE=1
 fi
 
@@ -238,86 +238,86 @@ if [ -z "$UPGRADE" ]; then
   fi
 
   # Display used environment variables
-  echo " ${ORANGE}admin email${NC} → using ${GREEN}$MY_EMAIL_ADDRESS${NC}"
-  echo " ${ORANGE}domain name${NC} → using ${GREEN}$MY_DOMAIN${NC}"
+  printf "%b" " ${ORANGE}admin email${NC} → using ${GREEN}$MY_EMAIL_ADDRESS${NC}\n"
+  printf "%b" " ${ORANGE}domain name${NC} → using ${GREEN}$MY_DOMAIN${NC}\n"
 
   # Step 2
-  echo "\nPreparing environment \\"
+  printf "%b" "\nPreparing environment \ \n"
 
   # Docker: make sure a non-root docker user system exists
-  echo -n "Creating a non-root docker user system ... "
+  printf "%b" "Creating a non-root docker user system ... "
   useradd >/dev/null 2>&1 -r -M -g docker docker # redirect out message if user already exists
-  echo $DONE
+  printf "%b" "$DONE\n"
 fi
 
 # Other architectures than x86_64
 if [ -z "`uname -a | grep -o "x86_64"`" ]; then
-  echo "$WARNING: Compose Binary can't be installed on your architecture"
+  printf "%b" "$WARNING: Compose Binary can't be installed on your architecture\n"
   COMPOSE="docker-compose"
   if ! has "$COMPOSE"; then
-    echo "$ERROR: Unfortunately docker-compose is not installed on your system"
+    printf "%b" "$ERROR: Unfortunately docker-compose is not installed on your system\n"
     exit 1
   else
     compose_current_release=`get_current_release "$COMPOSE -v"`
-    echo " → using system version ${GREEN}$compose_current_release${NC}"
+    printf "%b" " → using system version ${GREEN}$compose_current_release${NC}\n"
   fi
 else
   # Install or upgrade docker-compose
-  echo -n "Checking latest release of Compose     ... "
+  printf "%b" "Checking latest release of Compose     ... "
   compose_latest_release=`get_latest_release_name "docker/compose"`
-  [ -z "$compose_latest_release" ] && echo "${RED}Cannot resolve GitHub releases URL${NC}" && exit 1
-  echo $DONE
+  [ -z "$compose_latest_release" ] && printf "%b" "${RED}Cannot resolve GitHub releases URL${NC}" && exit 1
+  printf "%b" "$DONE\n"
 
   if ! has "$COMPOSE"; then
-    echo -n "Installing Docker Compose $compose_latest_release       ... "
+    printf "%b" "Installing Docker Compose $compose_latest_release       ... "
     get_docker_compose "$compose_latest_release"
-    echo $DONE
-    echo " → into ${ORANGE}$COMPOSE${NC}"
+    printf "%b" "$DONE\n"
+    printf "%b" " → into ${ORANGE}$COMPOSE${NC}\n"
   else
     compose_current_release=`get_current_release "$COMPOSE -v"`
 
     if ! is_update "$compose_current_release" "$compose_latest_release"; then
-      echo -n "Upgrading Docker Compose               ... "
+      printf "%b" "Upgrading Docker Compose               ... "
       get_docker_compose "$compose_latest_release"
-      echo $DONE
-      echo " → from $compose_current_release to ${GREEN}$compose_latest_release${NC}"
+      printf "%b" "$DONE\n"
+      printf "%b" " → from $compose_current_release to ${GREEN}$compose_latest_release${NC}\n"
     else
-      echo " → using current version ${GREEN}$compose_current_release${NC}"
+      printf "%b" " → using current version ${GREEN}$compose_current_release${NC}\n"
     fi
   fi
 fi
 
 # Get latest peertube cli
-echo -n "Installing CLI into ${ORANGE}$CLI${NC} ... "
+printf "%b" "Installing CLI into ${ORANGE}$CLI${NC} ... "
 rm -f $CLI
 get_latest_file "/cli/peertube" "$CLI"
 chmod +x $CLI
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Stop here if upgrading docker-compose / CLI
 if [ ! -z "$UPGRADE" ]; then
-  echo "\n${GREEN}Docker Compose and the CLI are now successfully upgraded!${NC}\n"
+  printf "%b" "\n${GREEN}Docker Compose and the CLI are now successfully upgraded!${NC}\n\n"
   exit 1
 fi
 
 # Init workdir
-echo -n "Creating ${ORANGE}$WORKDIR${NC}                 ... "
+printf "%b" "Creating ${ORANGE}$WORKDIR${NC}                 ... "
 mkdir -p "$WORKDIR/docker-volume"
 cd "$WORKDIR"
 mkdir -p docker-volume/certbot # Init docker-volume and certbot directory
 mkdir -p docker-volume/nginx # Init nginx directory
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Randomize PostgreSQL username and password
-echo -n "Generating PostgreSQL credentials      ... "
+printf "%b" "Generating PostgreSQL credentials      ... "
 
 MY_POSTGRES_USERNAME="`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10`"
 MY_POSTGRES_PASSWORD=`date +%s | sha256sum | base64 | head -c 32`
 
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Create / override .env
-echo -n "Generating ${ORANGE}$WORKDIR/.env${NC}          ... "
+printf "%b" "Generating ${ORANGE}$WORKDIR/.env${NC}          ... "
 
 cat <<EOT > .env
 POSTGRES_USER=<MY POSTGRES USERNAME>
@@ -345,25 +345,25 @@ sed -i -e "s/<MY DOMAIN>/$MY_DOMAIN/g" .env
 sed -i -e "s/<MY POSTGRES USERNAME>/$MY_POSTGRES_USERNAME/g" .env
 sed -i -e "s/<MY POSTGRES PASSWORD>/$MY_POSTGRES_PASSWORD/g" .env
 
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Copy nginx config
-echo -n "Installing latest ${ORANGE}nginx${NC} config         ... "
+printf "%b" "Installing latest ${ORANGE}nginx${NC} config         ... "
 get_latest_file "/nginx/peertube" "docker-volume/nginx/peertube"
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Copy docker-compose files
-echo -n "Installing latest ${ORANGE}compose${NC} file         ... "
+printf "%b" "Installing latest ${ORANGE}compose${NC} file         ... "
 get_latest_file "/docker/docker-compose.yml" "docker-compose.yml"
-echo $DONE
+printf "%b" "$DONE\n"
 
 # chown on workdir
-echo -n "Assigning $WORKDIR ownership      ... "
+printf "%b" "Assigning $WORKDIR ownership      ... "
 chown -R docker:docker "$WORKDIR"
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Create / override systemd service
-echo -n "Generating systemd peertube.service    ... "
+printf "%b" "Generating systemd peertube.service    ... "
 
 cat <<EOT > $SERVICE_PATH
 [Unit]
@@ -385,29 +385,30 @@ ExecStop=$COMPOSE stop peertube
 WantedBy=multi-user.target
 EOT
 
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Enable peertube systemd service
-echo -n "Enabling systemd peertube.service      ... "
+printf "%b" "Enabling systemd peertube.service      ... "
 systemctl >/dev/null 2>&1 daemon-reload # redirect out possible errors
 systemctl >/dev/null 2>&1 enable peertube
-echo $DONE
+printf "%b" "$DONE\n"
 
 # Step 3 - Compose pull
-echo "\nPulling docker images \\"
+printf "%b" "\nPulling docker images \ \n"
 $COMPOSE pull
 
 # Step 4 - Generate the first SSL certificate using Let's Encrypt
-echo "\nGenerating SSL certificate using Let's Encrypt \\"
+printf "%b" "\nGenerating SSL certificate using Let's Encrypt \ \n"
 $CLI certbot:init
 
 # Step 5 - Compose Up
-echo "\nUp docker stack \\"
+printf "%b" "\nUp docker stack \ \n"
 $CLI stack:up
 systemctl start --no-block peertube # be sure start process does not block stdout
 
 # Success message
 cat <<EOF
+
                       ______                           _______         _
                  _   (_____ \                         (_______)       | |
   ____  _____  _| |_  _____) ) _____  _____   ____        _     _   _ | |__   _____
@@ -416,9 +417,10 @@ cat <<EOF
  \___ ||_____)   \__)|_|      |_____)|_____)|_|    (_)   |_|   |____/ |____/ |_____)
 (_____|
 
+
 EOF
 
-echo "${GREEN}The PeerTube docker stack is now successfully installed!${NC}"
+printf "%b" "${GREEN}The PeerTube docker stack is now successfully installed!${NC}\n"
 
 cat <<EOF
 
@@ -426,8 +428,6 @@ Get your admin credentials and DKIM DNS TXT Record with:
 
 EOF
 
-echo "  ${ORANGE}$ peertube get-admin-credentials${NC}"
-echo "  ${ORANGE}$ peertube postfix:get-dkim-record${NC}"
-
-echo ""
+printf "%b" "  ${ORANGE}$ peertube get-admin-credentials${NC}\n"
+printf "%b" "  ${ORANGE}$ peertube postfix:get-dkim-record${NC}\n\n"
 }
